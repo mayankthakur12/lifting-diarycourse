a72ab97d8a4b70221a60281024f79bab8873f2fe
mayankthakur12 <mayank.schumi@gmail.com>
2026-09-08 11:07:32 +0530

    project setup

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,5 +1,4 @@
-import "dotenv/config";
-import { drizzle } from "drizzle-orm/neon-http";
-import { dbRelations } from "./schema";
+import { drizzle } from 'drizzle-orm/neon-http';
+const db = drizzle(process.env.DATABASE_URL!);
 
-export const db = drizzle(process.env.DATABASE_URL!, { relations: dbRelations });
+export {db};
